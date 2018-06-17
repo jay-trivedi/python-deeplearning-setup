ebfbed3cde7c370b8ba2ac773ecb6c44fdd3b1ed
Soumendra Dhanee <soumendra@gmail.com>
2018-06-17 15:46:35 +0000

    fixes conventions

--- condaremote.sh
+++ condaremote.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "Setting up remote jupyter notebook server"
-source activate py36tfgpu
+source activate cv
 jupyter notebook --generate-config
+cd
 mkdir .certs
 cd .certs
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
 cd ~/.jupyter
 echo "c = get_config()" >> ./jupyter_notebook_config.py
-echo "c.NotebookApp.certfile = u'/home/soumendra/.certs/mycert.pem'" >> ./jupyter_notebook_config.py
+echo "c.NotebookApp.certfile = u'/home/ubuntu/.certs/mycert.pem'" >> ./jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '*'" >> ./jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ./jupyter_notebook_config.py
 echo "c.NotebookApp.port = 8888" >> ./jupyter_notebook_config.py
